edb1cb44cdd715d9ae0477d5a31ef1b4353ac7f6
BiCute <bicute@naver.com>
2022-11-20 22:13:03 +0900

    node.js last version

--- main.js
+++ main.js
@@ -1,186 +1,151 @@
 var http = require('http');
-//fs = file Sysyem
 var fs = require('fs');
-// require = 모듈을 가져와서 사용한다는 것을 선언
 var url = require('url');
-const qs = require('querystring');
+var qs = require('querystring');
+var path = require('path');  // 경로 보안
+var sanitizeHtml = require('sanitize-html')  // 코드 보안 (예민한 태그(스크립트 태그 등) 제거)  ₩
 
-function templateHTML(title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head> 
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-    </body>
-    </html>
-    `;
-}
-
-function templateList(files) {
-    var list = '<ul>';
-    var i = 0;
-    while (i < files.length) {
-        list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-        i++;
-    }
-    list = list + '</ul>';
-    return list;
-}
+var template = require('./lib/template.js');
+const { emitWarning } = require('process');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var pathName = url.parse(_url, true).pathname;
+    var pathname = url.parse(_url, true).pathname;
 
-    console.log(url.parse(_url, true));
-
-    if (pathName === '/') {
-        // 쿼리가 없다면 = 홈페이지라면
+    if (pathname === '/') {
         if (queryData.id === undefined) {
-            fs.readdir('./data', (error, files) => {
-                var title = 'welcome';
-                var description = 'hello, Node.js';
-                var list = templateList(files);
-                var template = templateHTML(
-                    title,
-                    list,
-                    `<h2>${title}</h2>
-                    <p>${description}</p>`,
-                    `<a href="/create">create</a>`,
-                );
-                response.writeHead(200); // 200 = 성공
-                response.end(template); // 화면에 보여줄 내용
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = template.list(filelist);
+                var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a>`);
+                response.writeHead(200);
+                response.end(html);
             });
         } else {
-            fs.readdir('./data', (error, files) => {
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+            fs.readdir('./data', function (error, filelist) {
+                // 보안관련 필터링
+                var filterdId = path.parse(queryData.id).base;
+                // fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                fs.readFile(`data/${filterdId}`, 'utf8', function (err, description) {
                     var title = queryData.id;
-                    var list = templateList(files);
-                    var template = templateHTML(
+                    var sanitizedTitle = sanitizeHtml(title);
+                    var sanitizedDescription = sanitizeHtml(description);
+                    var list = template.list(filelist);
+                    var html = template.HTML(
                         title,
                         list,
-                        `<h2>${title}</h2> <p>${description}</p>`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>
-                        <form action="delete_process" method="post" ">
-                            <input type="hidden" name="id" value="${title}">
+                        `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${sanitizedTitle}">update</a>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
                             <input type="submit" value="delete">
                         </form>`,
                     );
                     response.writeHead(200);
-                    response.end(template);
+                    response.end(html);
                 });
             });
         }
-    } else if (pathName === '/create') {
-        fs.readdir('./data', (error, files) => {
+    } else if (pathname === '/create') {
+        fs.readdir('./data', function (error, filelist) {
             var title = 'WEB - create';
-            var list = templateList(files);
-            var template = templateHTML(
+            var list = template.list(filelist);
+            var html = template.HTML(
                 title,
                 list,
-                ` 
+                `
                 <form action="/create_process" method="post">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="description" placeholder="description"></textarea></p>
-                <p><input type="submit"></p>
+                    <p><input type="text" name="title" placeholder="title"></p>
+                    <p>
+                        <textarea name="description" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
                 </form>
-                `,
+            `,
                 '',
             );
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
         });
-    } else if (pathName === '/create_process') {
+    } else if (pathname === '/create_process') {
         var body = '';
-        // 웹브라우저가
         request.on('data', function (data) {
             body = body + data;
-            if (body.length > 1e6) {
-                request.connection.destroy();
-            }
         });
         request.on('end', function () {
             var post = qs.parse(body);
             var title = post.title;
             var description = post.description;
-            // 파일 생성
             fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-                if (err) throw err;
-                response.writeHead(302, { Location: `?id=${title}` }); // 302 = 페이지 리다이렉션
+                response.writeHead(302, { Location: `/?id=${title}` });
                 response.end();
             });
         });
-    } else if (pathName === '/update') {
-        fs.readdir('./data', (error, files) => {
+    } else if (pathname === '/update') {
+        fs.readdir('./data', function (error, filelist) {
             fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                 var title = queryData.id;
-                var list = templateList(files);
-                var template = templateHTML(
+                var list = template.list(filelist);
+                var html = template.HTML(
                     title,
                     list,
                     `
                     <form action="/update_process" method="post">
-                        <input type="hidden" name="id" value="${title}" >
+                        <input type="hidden" name="id" value="${title}">
                         <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                        <p><textarea name="description" placeholder="description" >${description}</textarea></p>
-                        <p><input type="submit"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
                     </form>
                     `,
                     `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
                 );
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             });
         });
-    } else if (pathName === '/update_process') {
+    } else if (pathname === '/update_process') {
         var body = '';
-        // 웹브라우저가
         request.on('data', function (data) {
             body = body + data;
-            if (body.length > 1e6) {
-                request.connection.destroy();
-            }
         });
         request.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
             var title = post.title;
             var description = post.description;
-            fs.rename(`data/${id}`, `data/${title}`, error => {
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
                 fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-                    if (err) throw err;
-                    response.writeHead(302, { Location: `?id=${title}` }); // 302 = 페이지 리다이렉션
+                    response.writeHead(302, { Location: `/?id=${title}` });
                     response.end();
                 });
             });
         });
-    } else if (pathName === '/delete_process') {
+    } else if (pathname === '/delete_process') {
         var body = '';
         request.on('data', function (data) {
             body = body + data;
-            if (body.length > 1e6) {
-                request.connection.destroy();
-            }
         });
         request.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`, error => {
+            var filterdId = path.parse(id).base;
+            fs.unlink(`data/${filterdId}`, function (error) {
                 response.writeHead(302, { Location: `/` });
                 response.end();
             });
         });
     } else {
-        response.writeHead(404); // 404 = 파일을 찾을수 없음
-        response.end('Not Found');
+        response.writeHead(404);
+        response.end('Not found');
     }
 });
-
 app.listen(3000);
---
+++ nodejs/muse.js
@@ -0,0 +1,15 @@
+// var M = {
+//     v: 'v',
+//     f: function () {
+//         console.log(this.v);
+//     },
+// };
+
+// M.f();
+
+// 모듈 가져오기
+var mpart = require('./mpart.js')
+
+mpart.f();
+
+
